a005e7d1f577d72ad6cdbf3ba02113651b1cc4fc
ericnrolll <fdsindayen@up.edu.ph>
2025-02-26 11:55:22 +0800

    Finalized addAccount() function, added test.js and .gitignore

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { v4 } from 'uuid';
 import isEmail from 'validator/lib/isEmail.js';
+import { appendFileSync } from 'fs';
 
 function generateUniqueID(firstName, lastName) {
     var id = "";
@@ -18,21 +19,38 @@ function generateUniqueID(firstName, lastName) {
 }
 
 
-function addAccount(firstName, lastName, email, age) {
+function addAccount(input) {
+    var firstName = input[0];
+    var lastName = input[1];
+    var email = input[2];
+    var age = input[3];
+
 
     // check if all fields are present
-    if (firstName === undefined || lastName === undefined || email === null || age === null) {
+    if (firstName === undefined || lastName === undefined || email === undefined || age === undefined) {
+        return false;
+    }
+
+    // check if first name, last name, and email are non-empty strings
+    if (typeof(firstName) != "string" || typeof(lastName) != "string" || typeof(email) != "string" || firstName === "" || lastName === "" || email === "") {
         return false;
     }
 
     // validate email format
     if (!isEmail(email)) {
         return false;
+    }   
+
+    // check if age is at least 18
+    if (age < 18) {
+        return false;
     }
 
-
+    appendFileSync('users.txt', firstName + "," + lastName + "," + email + "," + age + "," + generateUniqueID(firstName, lastName) + "\n")
 
     return true;
 }
 
-console.log(addAccount("Frederick", "Sindayen", "fdsindayen@up.edu.ph"))
+
+
+export default { generateUniqueID, addAccount }
--- test.js
+++ test.js
@@ -1,3 +1,12 @@
+import account from './index.js'
 
 
 
+console.log(account.addAccount(["Tim", "Berners-Lee", "tim@w3c.com", 25]))
+console.log(account.addAccount(["Ted", "Nelson", "ted@w3c.com", 43]))
+console.log(account.addAccount(["Frederick", "Sindayen", "fdsindayen@up.edu.ph", 21]))
+console.log(account.addAccount(["Ted", "Nelson", "ted@w3c.com", 16])) // returns false (age < 18)
+console.log(account.addAccount(["", "Nelson", "ted@w3c.com", 43])) // returns false (empty string)
+console.log(account.addAccount(["Ted", 5, "ted@w3c.com", 43])) // returns false (non-string)
+console.log(account.addAccount(["Ted", true, "ted@w3c.com", 43])) // returns false (non-string)
+console.log(account.addAccount(["Ted", "Nelson", "ted", 43])) // returns false (invalid email)
